83b272aa492f5891cd3b97b7373824ca69d6cc38
Filip Andonovski <filip@coqtail.com>
2019-01-05 19:03:22 +0100

    TransactionStorage class added for persistence

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,10 +1,10 @@
 class Transaction
   attr_accessor :id, :amount, :account_number
 
-  def initialize(init_json)
-    @id = init_json[:id] || random_transaction_id
-    @amount = init_json[:amount].to_i
-    @account_number = init_json[:account_number].to_i
+  def initialize(init_hash)
+    @id = init_hash[:id] || random_transaction_id
+    @amount = init_hash[:amount].to_i
+    @account_number = init_hash[:account_number].to_i
   end
 
   private
---
+++ app/models/transactions_storage.rb
@@ -0,0 +1,25 @@
+require 'singleton'
+class TransactionsStorage
+  include Singleton
+
+  def initialize
+    transactions_data = [{id: "TR3506988578", amount: 2000, account_number: 3506988578},
+                         {id: "TR3506988378", amount: 1460, account_number: 3506988422},
+                         {id: "TR3506988532", amount: -840, account_number: 3506988578},
+                         {id: "TR3506988422", amount: 2100, account_number: 3506988378}]
+    @transactions = transactions_data.map do |transaction_hash|
+      Transaction.new(transaction_hash)
+    end
+  end
+
+  def create_account_transaction!(account_number, amount)
+    new_transaction = Transaction.new({amount: amount, account_number: account_number})
+    @transactions << new_transaction
+    new_transaction
+  end
+
+  def get_account_transactions(account_number)
+    @transactions.select {|transaction| transaction.account_number == account_number.to_i}
+  end
+
+end
